f0bf1fbf7db6ec61696f4f09338ebb77fbe1d2ec
Hashim-ali02 <hashim.ali2026@gmail.com>
2025-07-19 23:58:41 +0100

    Log connected client and respond with 200 OK status

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
+    # Create a socket that listens on port 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    
+    # Block until we receive an incoming connection
+    connection, address = server_socket.accept()
+    # Log the address of the connected client
+    print(f"accepted connection from {address}")
+    # Send HTTP 200 OK response to the client
+    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
