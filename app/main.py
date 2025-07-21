6ebd057e18721440d40724ae302d9b7c6c04f51f
Hashim-ali02 <hashim.ali2026@gmail.com>
2025-07-21 20:55:58 +0100

    Split request data into method, path and version for easier handling and validation

--- app/main.py
+++ app/main.py
@@ -10,14 +10,17 @@ def main():
     # Log the address of the connected client
     print(f"accepted connection from {address}")
 
-    # Read the request from the client
-    request = connection.recv(1024).decode()
+    # Parse the request from the client
     
-    # Check if the request is a GET request for the root path
-    if request.startswith("GET / HTTP/1.1"):
+    data = connection.recv(1024).decode()
+    request = data.split("\r\n")[0]
+    method, path, version = request.split(" ")
+
+    # Check if method, path, and version are valid
+    if method == "GET" and path == "/" and version == "HTTP/1.1": 
         response = "HTTP/1.1 200 OK\r\n\r\n"
-    elif request.startswith("GET /echo/"):
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request[10:])}\r\n\r\n{request[10:]}"        
+    elif method == "GET" and path == "/echo/":
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request[6:])}\r\n\r\n{request[6:]}"        
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
