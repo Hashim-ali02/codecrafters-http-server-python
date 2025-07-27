8759e64917089ed772d671dcc3d32e65c41fa072
Hashim-ali02 <hashim.ali2026@gmail.com>
2025-07-27 22:26:51 +0100

    Implemented separate function to handle both accepted and unaccepted encodings

--- app/main.py
+++ app/main.py
@@ -30,6 +30,12 @@ def start_server():
         thread = threading.Thread(target=handleconnection, args=(connection, address))
         thread.start()
 
+def encoded_response(encoding):
+    acceptable_encodings = ["gzip"]
+    if encoding in acceptable_encodings:
+        return f"HTTP/1.1 200 OK\r\nContent-Encoding: {parsed_headers["Accept-Encoding"]}\r\nContent-Type: text/plain\r\nContent-Length: Size of compressed body to be implemented\r\n\r\n"
+    else:
+        return = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
         
 def handleconnection(connection, address):      
     # Parse the request from the client
@@ -48,7 +54,7 @@ def handleconnection(connection, address):
     if method == "GET" and path == "/" and version == "HTTP/1.1": 
         response = "HTTP/1.1 200 OK\r\n\r\n"
     elif method == "GET" and "Accept-Encoding" in parsed_headers:
-        response = f"HTTP/1.1 200 OK\r\nContent-Encoding: {parsed_headers["Accept-Encoding"]}\r\nContent-Type: text/plain\r\nContent-Length: Size of compressed body to be implemented\r\n\r\n"
+        response = encoded_response(parsed_headers["Accept-Encoding"])
     elif method == "GET" and path.startswith("/echo/"):
         response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
     elif method == "GET" and path == "/user-agent":
